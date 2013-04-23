60fb72b86226d5864be9088653feada9b1f2a598
Jarlin Brains <german_tov@hotmail.com>
2013-04-23 15:23:14 -0500

    The initial page with the canvas and the javascript for drawing in it

---
+++ main.js
@@ -0,0 +1,20 @@
+setup = function(){
+
+	var c = document.getElementById("cvs");
+	var context1 = c.getContext("2d");
+
+	c.width = window.innerWidth;
+	c.height = window.innerHeight;
+
+	var img = new Image();
+	
+	img.onload = function(){
+		console.log("IMAGE!!!");
+		context1.drawImage(img, 0, 0);
+	}
+	
+	img.src = 'dawg.gif';
+	
+}
+
+setup();
